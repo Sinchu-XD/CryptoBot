ad072241b359e9da5d6347bb13b5aae5fdd0319b
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-22 00:09:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
-from pyrogram import Client
+from pyrogram import Client, idle
+import asyncio
 from Config import API_ID, API_HASH, BOT_TOKEN
 from Utils.Db import init_db
+
 import Handlers.Price
 import Handlers.PortFolio
 
@@ -9,6 +11,26 @@ app = Client("CryptoBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 @app.on_message()
 async def start_handler(_, message):
     if message.text == "/start":
-        await message.reply("👋 Welcome to CryptoBot!\nUse /price BTC, /add, /portfolio, /nft(soon), /whale(soon)")
+        await message.reply(
+            "👋 Welcome to CryptoBot!\n\n"
+            "Commands:\n"
+            "• /price BTC\n"
+            "• /add\n"
+            "• /portfolio\n"
+            "• /nft (Soon)\n"
+            "• /whale (Soon)"
+        )
 
-app.run(init_db())
+async def main():
+    await init_db()
+    await app.start()
+    print("✅ Bot Started")
+    await idle()
+    await app.stop()
+    await asyncio.get_event_loop().create_future()
+
+if __name__ == "__main__":
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except KeyboardInterrupt:
+        print("🛑 Bot stopped manually.")
